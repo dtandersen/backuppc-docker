75c0d6b14f11708b911fcf92565f5e07ee4563fa
vagrant <vagrant@localhost.localdomain>
2018-11-11 07:47:10 +0000

    probably too many permissions

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -Eeo pipefail
 
-#if [ "$1" = 'supervisord' ] && [ "$(id -u)" = '0' ]; then
-#  mkdir -p "$DATA"
-#  chown -R backuppc "$DATA"
-#  chmod 700 "$DATA"
+if [ "$1" = 'supervisord' ] && [ "$(id -u)" = '0' ]; then
+  mkdir -p "$DATA"
+  chown -R 102:102 "$DATA"
+  chmod -R 777 "$DATA"
+  chmod 777 "$DATA"
+#  chmod -R a+rwX "$DATA"
+#  chmod 771 "$DATA"
 #
 #  exec su-exec backuppc "$BASH_SOURCE" "$@"
-#fi
+fi
 if [ ! -f /etc/backuppc/config.pl ]; then
   cp /etc/backuppc.sample/config.pl /etc/backuppc/config.pl
 fi
@@ -20,11 +23,11 @@ chown -R 102:102  /home/backuppc/.ssh
 #  cp /etc/backuppc.sample/hosts /etc/backuppc/hosts
 #fi
 
-if [ "$1" = 'supervisord' ]; then
-  mkdir -p "$DATA"
-  chown -R "$(id -u)" "$DATA"
-  chmod 700 "$DATA"
-fi
+#if [ "$1" = 'supervisord' ]; then
+#  mkdir -p "$DATA"
+#  chown -R "$(id -u)" "$DATA"
+#  chmod 777 "$DATA"
+#fi
 
 id
 pwd
